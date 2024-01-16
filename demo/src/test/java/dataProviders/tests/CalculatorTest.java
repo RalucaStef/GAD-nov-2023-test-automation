5425d1810b6274eb3c8fc9efec6945c1641f7533
Ralu <94392367+RalucaStef@users.noreply.github.com>
2024-01-16 20:56:16 +0200

    add:search on google, open first link

--- demo/src/test/java/dataProviders/tests/CalculatorTest.java
+++ demo/src/test/java/dataProviders/tests/CalculatorTest.java
@@ -1,6 +1,8 @@
 package dataProviders.tests;
 
 import com.calculator.Calculator;
+
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -12,9 +14,16 @@ public class CalculatorTest {
         calculator = new Calculator();
     }
 
-    @Test
-    public void testAddingPositiveNumbers()
+     @Test
+    public void  testAddingPositiveNumbers()
     {
-        
+        System.out.println("Verify that sum:" + calculator.compute(4, 5, "+") + " is equal to 9");
+        Assert.assertEquals(calculator.compute(4, 5, "+"), 9);
+    }
+
+    @Test(dataProvider = "calculatorPositiveDataProvider", dataProviderClass = CalculatorDataProviders.class)
+    public void testOperationsUsingPositiveNumbers(double d1, double d2, String operation, double expected)
+    {
+        Assert.assertEquals(calculator.compute(d1, d2, operation), expected, 0.00001);
     }
 }
